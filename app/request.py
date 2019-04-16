d17db8d3af03bc25d6576ab712e3bd2600f5932f
NelsonKioko <kiokonelson2@gmail.com>
2019-04-16 09:05:47 +0300

    getting url

--- app/request.py
+++ app/request.py
@@ -8,7 +8,7 @@ articles_base_url = None
 def configure_request(app):
 
     global api_key,base_url,articles_base_url
-    api_key = app.config['NEWS_API_KEY']
+    api_key = app.config['NEWS_API_KEY'] 
     base_url = app.config['NEWS_API_BASE_URL']
     articles_base_url = app.config['ARTICLES_API_BASE_URL']
 
